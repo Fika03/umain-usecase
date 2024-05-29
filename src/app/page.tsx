a46c906eba31a99956680742013dcbb59d9a3a1d
Fika03 <filipkarlstrom03@gmail.com>
2024-05-29 11:34:09 +0200

    fetched restaurants

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-import Image from "next/image";
+import RestaurantCard from "@/components/RestaurantCard";
+import { IRestaurant } from "@/models/IRestaurant";
+import { getRestaraunts } from "@/utils/getRestaurants";
 
-export default function Home() {
+export default async function Home() {
+  const res = await getRestaraunts();
+
+  const restaurants: IRestaurant[] = res.restaurants;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="bg-gray-100 flex min-h-screen flex-col items-center justify-between p-24">
+      <section>
+        <h2 className=" text-xl">Restaurant's</h2>
+        {restaurants.map((restaurant) => (
+          <RestaurantCard restaurant={restaurant} key={restaurant.id} />
+        ))}
+      </section>
+    </main>
   );
 }
